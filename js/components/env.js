f66df2dd8b0c9a84770ffd06ec54297da89fbba2
JuanFelipeSolanoAnteliz <juanfesolant0906@gmail.com>
2024-06-19 17:29:13 -0500

    chore: :truck: actualizacion de headers
    se pusieron headers nuevo con el fin de tener un total de 100 peticiones al momento de la revision por parte del docente y evitar errores en el porgreso

--- js/components/env.js
+++ js/components/env.js
@@ -1,5 +1,5 @@
 
 export const headers = {
-    'x-rapidapi-key': '94579ced7dmshb88814929b244e8p12ce3bjsna975fd565998',
+    'x-rapidapi-key': '0ba3d97123msh81f36363c9170e4p154c41jsn5038760d9f61',
     'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
 };

58b890ccd44b9c5d5c41e7d589258041f23465ec
JuanFelipeSolanoAnteliz <160759957+JuanFelipeSolanoAnteliz@users.noreply.github.com>
2024-06-24 20:17:24 -0500

    chore: :chore: Links image footer replaced
    the links to the images in the index footer and the headers of the endpoint api was updated

--- js/components/env.js
+++ js/components/env.js
@@ -1,5 +1,5 @@
 
 export const headers = {
-    'x-rapidapi-key': '35f9cfbab9msh58addc72ed65b20p18bfdcjsn1d026e51878b',
-    'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
+		'x-rapidapi-key': 'a906e6a123msh6302b01e4849184p1d6078jsnb69ccbf59727',
+		'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
 };

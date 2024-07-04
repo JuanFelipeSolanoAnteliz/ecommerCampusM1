883cc64b9ec34350f41394cf41e512fca778e5af
JuanFelipeSolanoAnteliz <160759957+JuanFelipeSolanoAnteliz@users.noreply.github.com>
2024-07-04 06:36:37 -0500

    chore: :truck: Update API headers
    the API headres was updated due to someone was watching mi eccomerce and expend all of my API requests and I not even was notice that

--- js/components/env.js
+++ js/components/env.js
@@ -1,5 +1,5 @@
 
 export const headers = {
-		'x-rapidapi-key': '6858cfcc69msh916f7f40646b2fap1e9a04jsncb996af25056',
+		'x-rapidapi-key': '75769cb246mshdb429db1d6629f5p17ad30jsn33237e659cb7',
 		'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
 };

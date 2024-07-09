7ceb76c186d472e4744e4ea6cf30cb034bc979cc
JuanFelipeSolanoAnteliz <juanfesolant0906@gmail.com>
2024-07-09 06:53:04 -0500

    chore: :truck: Actualizacion de endpoints
    se modificaron los headers de el API al que se apunta para el consumo

--- js/components/env.js
+++ js/components/env.js
@@ -1,5 +1,5 @@
 
 export const headers = {
-		'x-rapidapi-key': '75769cb246mshdb429db1d6629f5p17ad30jsn33237e659cb7',
-		'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
+	'x-rapidapi-key': '84bc45fd92msh264b74a95a83835p13b8f1jsnf35fdfb63f10',
+	'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
 };

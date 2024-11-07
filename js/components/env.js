bc830d7e0ab532ab07e6cafcfe0b73419f10520c
JuanFelipeSolanoAnteliz <juanfesolant0906@gmail.com>
2024-11-06 19:48:04 -0500

    headers

--- js/components/env.js
+++ js/components/env.js
@@ -1,5 +1,5 @@
 
 export const headers = {
-	'x-rapidapi-key': '84bc45fd92msh264b74a95a83835p13b8f1jsnf35fdfb63f10',
+	'x-rapidapi-key': '5d93763397mshf75cf864705c77ep1646f8jsnfc6cfd6790bc',
 	'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
 };
